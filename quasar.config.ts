5587e5813effa6abc537a8d08f5a319e1a7f05a1
ymarcon <yannick.marcon@epfl.ch>
2025-10-08 15:46:54 +0200

    chore: open browser on dev run

--- quasar.config.ts
+++ quasar.config.ts
@@ -90,7 +90,7 @@ export default defineConfig(() => {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#devserver
     devServer: {
       // https: true,
-      open: false, // opens browser window automatically
+      open: true, // opens browser window automatically
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
